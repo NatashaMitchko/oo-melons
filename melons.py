7ddc1826d2ac0221333d38ec3ddad9d11fd55a75
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-13 17:42:00 -0700

    Object Oriented Melons and Salespeople

---
+++ melons.py
@@ -0,0 +1,70 @@
+"""Classes for melon orders."""
+import random
+
+class AbstractMelonOrder(object):
+    """An abstract base class that other Melon Orders inherit from."""
+    def __init__(self, species, qty, order_type, tax):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+
+    def get_total(self):
+        """Calculate price, including tax."""
+        base_price = get_base_price()
+
+        if self.species == "Christmas":
+            base_price = base_price * 1.5
+
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+
+    def get_base_price(self):
+        """ Chooses random base price between 5 and 9 for splurge pricing"""
+        return random.randint(5,9)
+
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+        super(DomesticMelonOrder, self).__init__(species, qty, "domestic", 0.08)
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes."""
+        super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)
+        self.country_code = country_code
+
+
+    def get_total(self):
+        if self.qty > 10:
+            return super(InternationalMelonOrder, self).get_total()
+        else:
+            return super(InternationalMelonOrder, self).get_total() + 3
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """No tax on government melons"""
+    def __init__(self, species, qty):
+        super(GovernmentMelonOrder, self).__init__(species, qty, "government", 0)
+        self.passed_inspection = False
+
+    def mark_inspection(self, passed):
+        self.passed_inspection = passed
